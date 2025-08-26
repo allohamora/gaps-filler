673b47fcf4f8b0c1627e2ce068131e935e27da69
allohamora <herman30698@gmail.com>
2025-08-26 13:22:39 +0300

    feat(client): add navigation

--- apps/client/src/app.tsx
+++ apps/client/src/app.tsx
@@ -3,9 +3,11 @@ import { Button } from './components/ui/button';
 
 export const App: FC = () => {
   return (
-    <div>
-      <Button>test</Button>
-      <div>Hello world!</div>
+    <div className="flex items-center justify-center pt-10">
+      <div>
+        <Button>test</Button>
+        <div>Hello world!</div>
+      </div>
     </div>
   );
 };
--- apps/client/src/routes/__root.tsx
+++ apps/client/src/routes/__root.tsx
@@ -5,7 +5,20 @@ import { TanStackRouterDevtools } from '@tanstack/react-router-devtools';
 export const Route = createRootRoute({
   component: () => (
     <ThemeProvider defaultTheme="dark">
-      <Outlet />
+      <div className="flex min-h-screen min-w-full flex-col">
+        <nav className="bg-background/95 supports-[backdrop-filter]:bg-background/60 sticky top-0 z-40 w-full border-b backdrop-blur">
+          <div className="container flex h-14 items-center px-4">
+            <a href="/" className="flex items-center space-x-2">
+              <img src="/favicon.svg" alt="Gaps Filler Logo" className="size-6" />
+              <span className="font-bold">Gaps Filler</span>
+            </a>
+          </div>
+        </nav>
+
+        <main>
+          <Outlet />
+        </main>
+      </div>
       <TanStackRouterDevtools />
     </ThemeProvider>
   ),
